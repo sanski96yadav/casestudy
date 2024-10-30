82cef82ae79527b2aa5be52e8ce25d8a3f1a8f62
sanski96yadav <sanskruti96yadav@gmail.com>
2024-10-30 23:46:31 +0100

    Update fact_meta_spend_daily.sql

--- load&transformation/dwhlayer/mart/meta/fact_meta_spend_daily.sql
+++ load&transformation/dwhlayer/mart/meta/fact_meta_spend_daily.sql
@@ -1,8 +1,8 @@
 CREATE TABLE mart.fact_meta_spend_daily AS 
 SELECT
-   date_start AS pk_date,
+   date AS pk_date,
    SUM(spend) AS ad_spend 
 FROM
-   raw.raw_meta_campaign 
+   staging.meta_spend_data_prep
 GROUP BY
    1
