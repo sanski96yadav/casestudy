893160d215399fb9c22094e618ab5dbed9ce6d28
sanski96yadav <sanskruti96yadav@gmail.com>
2024-10-31 00:42:07 +0100

    Update fact_marketing_efficiency_daily.sql

--- load&transformation/dwhlayer/mart/combined/fact_marketing_efficiency_daily.sql
+++ load&transformation/dwhlayer/mart/combined/fact_marketing_efficiency_daily.sql
@@ -15,6 +15,6 @@ SELECT
    net_revenue 
 FROM
    mart.fact_meta_spend_daily AS m 
-   LEFT JOIN
+   LEFT JOIN --- left joined meta spend table because the aim is to analyze marketing efficiency and therefore all the marketing data should be included
       mart.fact_order_daily AS o 
-      ON m.pk_date = o.pk_created_at
+      ON m.pk_date = o.pk_created_at--- as both the tables have date as common key and we want data on daily level, the 2 tables are joined on date column
