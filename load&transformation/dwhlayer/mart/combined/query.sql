b0b6769993e72474e72481b6c4c244297cfcb94f
sanski96yadav <sanskruti96yadav@gmail.com>
2024-10-30 21:41:46 +0100

    Update query.sql

--- load&transformation/dwhlayer/mart/combined/query.sql
+++ load&transformation/dwhlayer/mart/combined/query.sql
@@ -1,1 +1,20 @@
-
+CREATE TABLE mart.fact_marketing_efficiency_daily AS 
+SELECT
+   pk_date AS pk_date,
+   ad_spend,
+   count_new_customers,
+   count_return_customers,
+   gross_merchandise_value,
+   discount_value,
+   shipping_revenue,
+   gross_revenue,
+   return_value,
+   taxes,
+   net_revenue_new,
+   net_revenue_return,
+   net_revenue 
+FROM
+   mart.fact_meta_spend_daily AS m 
+   LEFT JOIN
+      mart.fact_order_daily AS o 
+      ON m.pk_date = o.pk_created_at
