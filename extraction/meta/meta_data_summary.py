5e4dcfaf665f5f4cfce9b46b7c3f8a58f2e06b4c
sanski96yadav <sanskruti96yadav@gmail.com>
2024-11-01 00:17:53 +0100

    Update meta_data_summary.py

--- extraction/meta/meta_data_summary.py
+++ extraction/meta/meta_data_summary.py
@@ -5,33 +5,32 @@ import pandas as pd
 import psycopg2
 import csv
 
-# Define your ad account ID and access token
-ad_account_id = '******'  # Replace with your actual ad account ID
-access_token = '*****'  # Replace with your actual access token
+
+ad_account_id = '******' 
+access_token = '*****' 
 
 # Define the fields you want to fetch
 fields = [
     'account_currency,account_name,impressions,spend'
 ]
 
-# Construct the URL
+# URL to fetch data from API
 url = f"https://graph.facebook.com/v21.0/{ad_account_id}/insights?&date_preset=maximum"
 params = {
     'fields': ','.join(fields),
     'access_token': access_token,
 }
 
-# Make the GET request to fetch data
+# GET request to fetch data from API
 response = requests.get(url, params=params)
 # to see all columns with values
 pd.set_option('display.max_columns', None)
 
-# Convert the response to JSON
+# Converts the response to JSON
 data = response.json()
 
-# Load data into a pandas DataFrame
+# Loads data into a pandas DataFrame
 df = pd.DataFrame(data['data'])
 
-# Display the data in a tabular format
-
+# Gives csv file
 df.to_csv('meta_data_summary.csv', index=False)
